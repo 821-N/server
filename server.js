fff762a3519e41e801e452f107b03bdfc623b2be
12Me21 <12Me21.mc@gmail.com>
2020-03-11 09:38:23 -0400

    improve error messages

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(BodyParser.text({type: "text/*"}));
 app.use(BodyParser.json());
 
 var keys = {
-	password: "12Me21"
-}
+	password: "TestUser",
+};
 
 function lookupAuth(auth) {
 	if (keys[auth] === undefined) {
@@ -24,34 +24,44 @@ function lookupAuth(auth) {
 	return keys[auth];
 }
 
-app.get('/', function (req, res) {
-	var room = req.query.room;
-	if (room === undefined) {
-		res.status(404).send("missing room parameter");
-		return;
-	}
+// returns room, user, error
+function process(req) {
 	var auth = req.headers.auth;
-	if (auth === undefined) {
-		res.status(404).send("missing auth header");
+	if (!auth)
+		return [null, null, "Missing auth token"];
+	var user = lookupAuth(auth);
+	if (!user)
+		return [null, null, "Invalid auth token"];
+	var room = req.query.room;
+	if (room == undefined)
+		return [null, null, "Missing room query parameter"]
+	var room = getRoom(room)
+	if (!room)
+		return [null, null, "Invalid room"];
+	return [room, user, null];
+}
+
+app.get('/', function (req, res) {
+	var [room, user, err] = process(req);
+	if (err) {
+		// todo: better status codes
+		res.status(404);
+		res.send(err);
 		return;
 	}
-
-	room = getRoom(room);
 	
 	var id = req.headers.id;
-	if (id === undefined) {
-		res.set("id", room.nextId());
-		res.json(room.fromId());
+	
+	var callback = (messages, id) => {
+		res.set('id', id);
+		res.json(messages);
 		res.end();
-	} else {
-		room.addResponse(res, id);
 	}
+	callback.id = id;
+	room.addResponse(callback);
 });
 
-
-
 var rooms = {};
-
 function getRoom(name) {
 	if (rooms[name])
 		return rooms[name];
@@ -60,33 +70,22 @@ function getRoom(name) {
 
 // send
 app.post('/', function (req, res) {
-	console.log("post", req.body);
-	console.log(req.headers);
-	var room = req.query.room;
-	if (room === undefined) {
-		res.status(404).send("missing room parameter");
-		return;
-	}
-	var auth = req.headers.auth;
-	if (auth === undefined) {
-		res.status(404).send("missing auth header");
+	var [room, user, err] = process(req);
+	if (err) {
+		// todo: better status codes
+		res.status(404);
+		res.send(err);
 		return;
 	}
 	var message = req.body;
 	if (message === undefined) {
-		res.status(404).send("missing body");
+		res.status(404).send("Missing message");
 		return;
 	}
-	// check auth
-	if (auth !== "password") {
-		res.status(404).send("bad auth");
-		return;
-	}
-	room = getRoom(room);
-	room.gotMessage(message);
 	res.end();
+	room.post(message);
 });
 
-app.listen(9998, function () {
+app.listen(9998, () => {
 	console.log("starting")
 })
