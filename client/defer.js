875a883d6313e84641f742bd8539c8802a22a683
821-N <nathaniel.marofsky@gmail.com>
2020-03-18 11:50:08 -0400

    update page layout

--- client/defer.js
+++ client/defer.js
@@ -1,4 +1,5 @@
 var auth;
+var user;
 var cancel = [function(){}];
 /*login("testuser2", "password", function(a) {
 	auth = a;
@@ -6,6 +7,37 @@ var cancel = [function(){}];
 		$output.textContent = message + "\n" + $output.textContent;
 	}, cancel);
 })*/
+
+function setAuth(a, u) {
+	auth = a;
+	user = u;
+	if (a) {
+		$logged_out.setAttribute("hidden","hidden");
+		$logged_in.removeAttribute("hidden");
+		$myself.textContent = "logged in as "+user;
+	} else {
+		$logged_in.setAttribute("hidden","hidden");
+		$logged_out.removeAttribute("hidden");
+		$myself.textContent = "not logged in";
+	}
+}
+
+setAuth(null);
+
+$logout.onclick = function() {
+	logout(auth, error);
+	setAuth(null);
+}
+
+$login.onclick = function() {
+	login($username.value, $password.value, function(a, err){
+		if(a){
+			setAuth(a, $username.value);
+		}else{
+			error('Error logging in: '+err);
+		}
+	});
+}
 
 $send.onclick = function() {
 	sendMessage(auth, $room.value, $input.value, function(err){
